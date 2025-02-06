141346defea63c285cec0428a0d33a50b16db8e1
KashanMirxa01 <164162562+KashanMirxa01@users.noreply.github.com>
2025-02-06 21:57:18 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
+  packname: 'Arshan',
+  author: 'Arshan Mirxa',
+  botName: "Arshan Mirxa",
   botOwner: 'Arshan Mirxa', // Your name
   ownerNumber: '923058449293', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
