86ec7a25a114367baaaafc7147f29680cd1fbdc1
KashanMirxa01 <164162562+KashanMirxa01@users.noreply.github.com>
2025-02-06 21:53:00 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Arshan Mirxa', // Your name
+  ownerNumber: '923058449293', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
